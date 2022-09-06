93caebf3f07cd2b6f1f27011cf870193bfbddada
Pawanihansi <pawani.wit@icloud.com>
2022-09-06 13:50:41 +0530

    add Scrollview

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 //import { StatusBar } from 'expo-status-bar';
 import React,{useState} from 'react';
-import { StyleSheet, Text, View,Button } from 'react-native';
+import { StyleSheet, Text, View,Button, ScrollView } from 'react-native';
 
 export default function App() {
   const[name,setName] =useState('Tanisha');
@@ -11,13 +11,33 @@ export default function App() {
     setPerson({name:'shani', age: 30});
   }
 
+
+  const[people,setPeople]=useState([
+    {season:'summer', key:'1'},
+    {season:'winter', key:'2'},
+    {season:'autumn', key:'3'},
+    {season:'spring', key:'4'}
+  ])
   return (
     <View style={styles.container}>
+      
       <Text>My name is {name}</Text>
       <Text>His name is{person.name} and her age is {person.age}</Text>
       <View style={styles.buttonContainer}>
         <Button title='update state' onPress={clickHandler}/>
       </View>
+      
+      
+      <ScrollView>
+        {people.map(item =>(
+            <View key={item.key}>
+              <Text style={styles.item}>{item.season}</Text>
+              </View>
+          ))}
+        
+      </ScrollView>
+
+      
     </View>
   );
 }
